e811d5d5c2c19d9906b47fd1571457d50148bebc
INTEGER\alamarz <alamarz@inpost.pl>
2021-05-14 08:08:16 +0200

    Initial commit

---
+++ TestClrApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestClrApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            await Task.Run(async () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"{i}");
+                    await Task.Delay(1000);
+                }
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
